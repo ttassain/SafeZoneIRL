fe592e65301453919bf411895752e2032de0a07b
Thierry TASSAIN <thierry.tassain@orange.fr>
2025-03-12 18:36:19 +0100

    promicro -> ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <Arduino.h>
-#include <Keyboard.h>
-#include <Mouse.h>
-#include <Keyboard_fr_FR.h>
 #include <Keypad.h>
 #include "Button2.h"
 #include <Ultrasonic.h>
@@ -22,38 +19,35 @@ char keys[ROWS][COLS] = {
   { '7', '8', '9' },
   { '*', '0', '#' }
 };
-byte rowPins[ROWS] = { 10, 16, 14, 15 };
-byte colPins[COLS] = { 7, 8, 9 };
+byte rowPins[ROWS] = { 32, 33, 18, 19 };
+byte colPins[COLS] = { 25, 26, 27 };
 
 Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // ULTRASONIC
-Ultrasonic ultrasonic(4, 5);	// An ultrasonic sensor HC-04 - TRIGGER / ECHO
+Ultrasonic ultrasonic(13, 14);	// An ultrasonic sensor HC-04 - TRIGGER / ECHO
 int distance;
 unsigned long lastDistanceUpdate = 0;
 unsigned long distanceDelay = 1000;
 
 // BUTTON2
-const uint8_t BUTTON_PICKUP = 6;
+const uint8_t BUTTON_PICKUP = 4;
 Button2 button;
 
 // LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// SERIAL1
+// SERIAL
 const byte bufferSize = 64;
 char inputBuffer[bufferSize];
 byte bufferIndex = 0;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  Serial1.begin(9600);
-  Serial1.println("READY !");
-
-  Keyboard.begin(KeyboardLayout_fr_FR);
-  Mouse.begin();
+  Serial2.begin(9600);
+  Serial2.println("READY !");
 
   button.begin(BUTTON_PICKUP);
   button.setPressedHandler(onButtonPressed);
@@ -66,7 +60,7 @@ void setup() {
   lcd.print("Safe Zone");
   lcd.setCursor(0, 1);
   lcd.print("Distance :");
-}
+} 
 
 void loop() {
   if (millis() - lastDistanceUpdate > distanceDelay) {
@@ -87,65 +81,66 @@ void loop() {
   if (customKey) {
     switch (customKey) {
       case '0':
-        Keyboard.write('0');
+        Serial2.println("0");
         break;
       case '1':
-          Keyboard.write('1');
+        Serial2.println("1");
         break;
       case '2':
-        Keyboard.write('2');
+        Serial2.println("2");
         break;
       case '3':
-        Keyboard.write('3');
+        Serial2.println("3");
         break;
       case '4':
-        Keyboard.write('4');
+        Serial2.println("4");
         break;
       case '5':
-        Keyboard.write('5');
+        Serial2.println("5");
         break;
       case '6':
-        Keyboard.write('6');
+        Serial2.println("6");
         break;
       case '7':
-        Keyboard.write('7');
+        Serial2.println("7");
         break;
       case '8':
-        Keyboard.write('8');
+        Serial2.println("8");
         break;
       case '9':
-        Keyboard.write('9');
+        Serial2.println("*");
         break;
       case '*':
-        Keyboard.write('*');
+        Serial2.println("*");
         break;
       case '#':
-        Keyboard.write('#');
+        Serial2.println("#");
         break;
     }
     //Serial.println(customKey);
+
     lcd.setCursor(15, 0);
     lcd.print(customKey);
   }
 
-  while (Serial1.available() > 0) {
-    char receivedChar = Serial1.read(); // Lire un caractère
+  while (Serial2.available() > 0) {
+    char receivedChar = Serial2.read(); // Lire un caractère
 
     // Si le caractère est un caractère de fin de ligne (\n), traiter la chaîne
     if (receivedChar == '\n' || receivedChar == '$') {
       inputBuffer[bufferIndex] = '\0'; // Terminer la chaîne avec un caractère nul
-      Serial.print("Chaîne reçue sur Serial1 : ");
+      //Serial.print("Chaîne reçue sur Serial2 : ");
       Serial.println(inputBuffer);
       bufferIndex = 0;
-      
-      Serial1.println("");
+
+      Serial2.println("");
 
       lcd.setCursor(0, 0);
       lcd.print(inputBuffer);
     } else {
       if (bufferIndex < bufferSize - 1) {
         inputBuffer[bufferIndex++] = receivedChar;
-        Serial1.print(receivedChar);
+        Serial2.print(receivedChar);
       }
     }
   }
@@ -156,7 +151,7 @@ void onButtonPressed(Button2 &btn) {
   switch (btn.getPin())
   {
     case BUTTON_PICKUP:
-      Keyboard.write('R');
+      Serial2.println("RAC");
       lcd.setCursor(11, 0);
       lcd.print("Rac");
       break;
@@ -169,7 +164,7 @@ void onButtonReleased(Button2 &btn) {
   switch (btn.getPin())
   {
     case BUTTON_PICKUP:
-      Keyboard.write('D');
+      Serial2.println("DEC");
       lcd.setCursor(11, 0);
       lcd.print("Dec");
       break;
